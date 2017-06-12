4542fa166ea9f18bd304fd89ee3ee90f5e16822f
Arnaud LECOMTE <arlecomt@e2r10p15.42.fr>
2017-06-12 12:33:53 +0200

    favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 app.use('/', index);
 app.use('/users', users);
+app.use('/signIn', signIn);
 
 
 // catch 404 and forward to error handler
