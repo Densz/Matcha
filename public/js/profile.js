d9b06ab04c6233367dbd7e9fe0a173fe0b830a20
Aeners <arlecomt@student.42.fr>
2017-07-29 02:50:13 +0200

    Upload picture done, image swipe on others users profile page ok

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,49 @@ var likeProfile = document.querySelector('.like');
 var dislikeProfile = document.querySelector('.dislike');
 var dislikeAfterLike = document.querySelector('#dislikeAfterLike');
 var div = document.querySelector('#swipe');
+var arrowNext = document.querySelector('.next-arrow');
+var arrowPrev = document.querySelector('.previous-arrow');
 
+if (arrowNext != null) {
+    arrowNext.addEventListener('click', function() {
+        var xhr = new XMLHttpRequest();
+        var currSrcPic = document.querySelector('.profile_picture');
+        var loginMatch = urlUserProfile();
+        var currPicName = currSrcPic.src.split("/");
+        console.log('currImgSrc -> ' + currPicName[4]);
+        xhr.open('POST', url() + '/home/nextPicture', true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send('indexCurrPic=' + currPicName[4] + '&loginMatch=' + loginMatch);
+        xhr.onload = function() {
+            if (xhr.readyState === xhr.DONE) {
+                if (xhr.status === 200 || xhr.status === 0) {
+                    var res = xhr.responseText;
+                    currSrcPic.src = res;
+                }
+            }
+        }
+    });
+}
+if (arrowPrev !== null) {
+    arrowPrev.addEventListener('click', function() {
+        var xhr = new XMLHttpRequest();
+        var currSrcPic = document.querySelector('.profile_picture');
+        var loginMatch = urlUserProfile();
+        var currPicName = currSrcPic.src.split("/");
+        console.log('currImgSrc -> ' + currPicName[4]);
+        xhr.open('POST', url() + '/home/previousPicture', true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send('indexCurrPic=' + currPicName[4] + '&loginMatch=' + loginMatch);
+        xhr.onload = function() {
+            if (xhr.readyState === xhr.DONE) {
+                if (xhr.status === 200 || xhr.status === 0) {
+                    var res = xhr.responseText;
+                    currSrcPic.src = res;
+                }
+            }
+        }
+    });
+}
 function urlUserProfile(){
     var url =  window.location.href;
     url = url.split("/");
