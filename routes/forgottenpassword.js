f13a2acf487385451451c1590fa50031afede13c
Densz <zheng.denis@gmail.com>
2017-07-29 04:58:16 +0200

    beginning of forgotten password

--- routes/forgottenpassword.js
+++ routes/forgottenpassword.js
@@ -4,20 +4,35 @@ const model = require('../core/models/database');
 
 router.get('/', function(req, res){
     let errors = req.session.errors;
+    let success = req.session.success;
 
     if (req.session.login) {
         res.redirect('/home');
     } else {
         req.session.errors = null;
+        req.session.success = null;
 
         res.render('forgottenpassword', {
             title: 'Matcha - Forgotten password',
-            errors: errors
+            errors: errors,
+            success: success
         });
     }
 });
 
 router.post('/submit', async function(req, res){
+    req.session.errors = [];
+    req.session.success = [];
+    let db = await model.connectToDatabase();
+    let email = await db.collection('users').findOne({ email: req.body.email });
+
+    if (email === null) {
+        req.session.errors.push({msg: "Email not found."});
+        res.redirect('/forgottenpassword');
+    } else {
+        req.session.success.push({msg: "Email sent."});
+        res.redirect('/forgottenpassword');
+    }
 });
 
 module.exports = router;
