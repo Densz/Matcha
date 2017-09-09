48c86438291e0bafbdbac4c82ff2ae3cd71fd971
Arnaud LECOMTE <arlecomt@e2r11p14.42.fr>
2017-09-09 17:50:22 +0200

    reset password

--- routes/resetpassword.js
+++ routes/resetpassword.js
@@ -5,6 +5,11 @@ const objectId = require('mongodb').ObjectID;
 const model = require('../core/models/database');
 const passwordHash = require('password-hash');
 
+router.get('/', (req, res) => {
+    req.session.errors = [];
+    req.session.errors.push({msg: 'No login found'});
+    res.redirect('/');
+});
 
 router.get('/:id', function(req, res){
     let errors = req.session.errors;
@@ -27,20 +32,26 @@ router.get('/:id', function(req, res){
 
 router.post('/:id/submit', async function(req, res){
     req.session.errors = [];
-    req.session.success = [];
-    let db = await model.connectToDatabase();
-    let user = await db.collection('users').findOne({_id: objectId(req.params.id)});
-
-    if (user === null) {
-        req.session.errors.push({msg: 'User not found'});
-        res.redirect('/resetpassword/' + req.params.id);        
-    } else if (req.body.newPassword === req.body.newPasswordConfirmation) {
-        await model.updateData('users', { _id: objectId(req.params.id) }, { $set: { password: passwordHash.generate(req.body.newPassword)} })
-        req.session.success.push({msg: 'You password has been changed'});
+    let checkObjID = new RegExp("^[0-9a-fA-F]{24}$");
+    if (checkObjID.test(req.params.id) === false) {
+        req.session.errors.push({msg: 'Page URL false'});
         res.redirect('/resetpassword/' + req.params.id);
     } else {
-        req.session.errors.push({msg: 'Password confirmation must match Password'});
-        res.redirect('/resetpassword/' + req.params.id);
+        req.session.success = [];
+        let db = await model.connectToDatabase();
+        let user = await db.collection('users').findOne({_id: objectId(req.params.id)});
+
+        if (user === null) {
+            req.session.errors.push({msg: 'User not found'});
+            res.redirect('/resetpassword/' + req.params.id);        
+        } else if (req.body.newPassword === req.body.newPasswordConfirmation) {
+            await model.updateData('users', { _id: objectId(req.params.id) }, { $set: { password: passwordHash.generate(req.body.newPassword)} })
+            req.session.success.push({msg: 'You password has been changed'});
+            res.redirect('/resetpassword/' + req.params.id);
+        } else {
+            req.session.errors.push({msg: 'Password confirmation must match Password'});
+            res.redirect('/resetpassword/' + req.params.id);
+        }
     }
 });
 
